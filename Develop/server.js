0b6041f5c0908b003cfe313813aa244b4a75ff9c
uchrissd <uchrissd@protonmail.com>
2020-02-11 22:13:15 -0800

    updated server file

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI);
 
